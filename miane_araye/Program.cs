75f356883e7f08f7b56602a856f4d56a658cb679
ma.ahmadi <m83115151@gmail.com>
2023-06-18 14:21:52 +0430

    Add project files.

---
+++ miane_araye/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miane_araye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = new double[50];
+            for (int i = 0; i < 50; i++)
+            {
+             Console.Write("element {0} = ", i + 1);
+
+              array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Calc(array);
+        }
+        private static void Calc(double[] array)
+        {
+            for (int i = 1; i <= 48; i++)
+            {
+                array[i] = (array[i - 1] + array[i] + array[i + 1]) / 3;
+            }
+            for (int i = 0; i <= 49; i++)
+            {
+                Console.WriteLine("new element {0} = {1}", i + 1, array[i]);
+            }
+        }
+    }
+}
